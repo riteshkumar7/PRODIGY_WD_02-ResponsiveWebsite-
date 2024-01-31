c9b6146145619ae399b68c4968911bf093625621
riteshkumar <riteshkumar8561@gmail.com>
2024-01-31 13:08:04 +0530

    responsivewebsite

---
+++ menu.js
@@ -0,0 +1,8 @@
+const showSidebar=()=>{
+    var navbar = document.querySelector(".navbar");
+    navbar.style.display = "block";
+}
+const hideSidebar=()=>{
+ var navbar = document.querySelector(".navbar");
+    navbar.style.display = "none";
+}
